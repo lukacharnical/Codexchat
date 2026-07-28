ded306df4915d9c1f05d7046be4257bc75c825ff
charnical <lukacharnical45@outlook.com>
2026-07-29 01:01:00 +0200

    Create ProjectsPage with project list and add button
    This file implements a ProjectsPage with a list of projects and a button to add new projects.

---
+++ app/flutter/lib/screens/projects.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ProjectsPage extends StatefulWidget {
+  const ProjectsPage({super.key});
+
+  @override
+  State<ProjectsPage> createState() => _ProjectsPageState();
+}
+
+class _ProjectsPageState extends State<ProjectsPage> {
+
+  List<String> projets = [
+    "🐍 Projet Python",
+    "🌐 Site HTML",
+    "➕ Projet C++",
+  ];
+
+  void ajouterProjet() {
+    setState(() {
+      projets.add("📁 Nouveau projet");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mes projets"),
+      ),
+
+      body: ListView.builder(
+        itemCount: projets.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: const Icon(Icons.code),
+              title: Text(projets[index]),
+              trailing: const Icon(Icons.edit),
+            ),
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: ajouterProjet,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
